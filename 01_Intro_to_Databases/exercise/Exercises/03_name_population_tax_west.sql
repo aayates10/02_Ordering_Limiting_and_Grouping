1a6d4084c11816d2af69ccbc6c71f357a224c716
Aaron Yates <aaronyates1@gmail.com>
2022-12-26 14:44:25 -0700

    Intro to Database

--- 01_Intro_to_Databases/exercise/Exercises/03_name_population_tax_west.sql
+++ 01_Intro_to_Databases/exercise/Exercises/03_name_population_tax_west.sql
@@ -1,2 +1,8 @@
 -- 3. The name, population, and sales tax of the states in the "West" census region (13 rows)
+SELECT
+state_name,population,sales_tax
+From 
+state
+WHERE 
+census_region ='West';
 
