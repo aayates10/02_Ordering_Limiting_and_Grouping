fcd80b6534c5c48f0a3e3592434e41e87a176aa0
Aaron Yates <aaronyates1@gmail.com>
2023-02-12 22:20:52 -0700

    Aaron Yates Server side API part 1

--- 13_Server_Side_APIs_Part_1/exercise/src/main/java/com/techelevator/auctions/controller/AuctionController.java
+++ 13_Server_Side_APIs_Part_1/exercise/src/main/java/com/techelevator/auctions/controller/AuctionController.java
@@ -2,7 +2,11 @@ package com.techelevator.auctions.controller;
 
 import com.techelevator.auctions.dao.AuctionDao;
 import com.techelevator.auctions.dao.MemoryAuctionDao;
+import com.techelevator.auctions.model.Auction;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/auctions")
@@ -13,5 +17,28 @@ public class AuctionController {
     public AuctionController() {
         this.dao = new MemoryAuctionDao();
     }
+    @RequestMapping(method = RequestMethod.POST)
+    public Auction create(@RequestBody Auction auction){
+        dao.create(auction);
+        return auction;
+    }
 
-}
+    @RequestMapping(path = "/{id}",method = RequestMethod.GET)
+    public Auction get(@PathVariable int id){
+        return dao.get(id);
+    }
+    @RequestMapping( method = RequestMethod.GET)
+    public List<Auction> list(@RequestParam(defaultValue = "") String title_like,
+                              @RequestParam(defaultValue = "-2")int currentBid_lte) {
+        if (title_like.isEmpty()&&currentBid_lte== -2) {
+            return dao.list();
+        } else if (title_like.isEmpty()) {
+            return dao.searchByPrice(currentBid_lte);
+        } else if (currentBid_lte == -2) {
+            return dao.searchByTitle(title_like);
+        }else
+        return dao.searchByTitleAndPrice(title_like,currentBid_lte);
+        }
+    }
+
+
